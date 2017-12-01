8cd62a48c87c88fb4ac96268ef8e3fe5553d6177
Randy <randylam87@gmail.com>
2017-11-30 23:11:23 -0800

    re-added start script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-
+console.log('Key: ',process.env.BR_API_KEY)
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
